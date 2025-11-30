f63bde4644f2d1f6a39f84c4d6bb2657c31cba3c
GibsonCS <gibson.cruzds@gmail.com>
2025-11-29 22:44:40 -0300

    feat(roles): create role entity

---
+++ src/main/java/br/com/codelift/ecomerce/infrastructure/persister/RoleEntity.java
@@ -0,0 +1,31 @@
+package br.com.codelift.ecomerce.infrastructure.persister;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+public class RoleEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String role;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
